dee601e676c032a5e17fd774d8b63dfa61a7c430
BenjiHansell <benji.hansell@outlook.com>
2017-08-25 15:11:15 +0100

    Change to the way constructors are called
    Previously an array was allocated at construction of the empty queue in which every element of the array was default constructed and then reassigned by push().  Now all space is allocated as an array of chars (which have no default constructor) and constructors (and now destructors too) are called explicitly for push() and pop().  This paves the way for implementation of an emplace() function.

--- static_queue.h
+++ static_queue.h
@@ -3,11 +3,29 @@
 
 #include <stddef.h>
 
+#if defined Arduino_h && !defined STD_LIB_AVAILABLE
+#define STD_LIB_UNAVAILABLE
+#endif
+// if using Arduino with an implementation of the C++ standard library
+// available, STD_LIB_AVAILABLE must be defined to prevent placement operator
+// new being	defined twice
+// if not using Arduino, the C++ standard library is assumed to be available,
+// otherwise STD_LIB_UNAVAILABLE must be defined before #include
+// <static_queue.h>
+
+#ifdef STD_LIB_UNAVAILABLE
+void *operator new(size_t size, void *ptr) {
+  return ptr;
+} // definition of placement new
+#else
+#include <new> // alternatively, placement new is already defined by the standard library
+#endif
+
 typedef size_t size_type;
 
 template <typename T, size_type max_size> class static_queue {
 public:
-  static_queue() : start_idx(0), _size(0) {}
+  static_queue() : start_idx(0), _size(0), data((T *)data_raw) {}
   bool empty() const { return size() == 0; }
   bool full() const { return size() == max_size; }
   void push(const T &val); // inserts a new element at the end of the queue if
@@ -20,7 +38,8 @@ public:
 private:
   size_type start_idx;
   size_type _size;
-  T data[max_size];
+  char data_raw[max_size * sizeof(T)];
+  T *const data; // might be able to make this a reference
 };
 
 template <typename T, size_type max_size>
@@ -28,18 +47,17 @@ void static_queue<T, max_size>::push(const T &val) {
   if (full())
     return; // if there is no space, push() will do nothing
   _size++;
-  back() = val;
+  new (&back()) T(val);
 }
 
 template <typename T, size_type max_size>
 void static_queue<T, max_size>::pop() {
   if (empty())
     return; // it is safe to call pop() on an empty queue
+  front().~T();
   _size--;
   start_idx = start_idx < max_size - 1 ? start_idx + 1 : 0;
 }
-// ideally I would want to call ~T() on the element being removed, but this
-// causes problems for future calls of push() which does not call a constructor
 
 // should not be called on an empty queue
 template <typename T, size_type max_size>
@@ -48,11 +66,9 @@ T &static_queue<T, max_size>::front() {
 }
 
 // should not be called on an empty queue
-template <typename T, size_type max_size> 
-T &static_queue<T, max_size>::back() {
+template <typename T, size_type max_size> T &static_queue<T, max_size>::back() {
   size_type end_idx = start_idx + size() - 1;
-  if(end_idx >= max_size) 
-    end_idx -= max_size;
+  end_idx %= max_size;
   return data[end_idx];
 }
 
